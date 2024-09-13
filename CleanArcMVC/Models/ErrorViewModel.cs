95d5c3dd63077efafe3e5d6fbd81abd1aade95c2
mohsen <71mohsenpsh@gmail.com>
2024-09-13 12:45:09 +0330

    add WebApp Project in Presentation layer

---
+++ CleanArcMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArcMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
