985b74597554df2f8a991e213b618fff8bd96263
mohsen <71mohsenpsh@gmail.com>
2024-09-13 12:53:32 +0330

    Implement the Domain layer

---
+++ CleanArc.Domain/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Domain.Models
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
